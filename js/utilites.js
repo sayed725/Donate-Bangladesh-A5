1d1b49e8ecc4b826cc4a3314d3b4bb456260ed48
sayed725 <ssayed72533@gmail.com>
2024-09-23 12:19:13 +0600

    added 3 designed modal

--- js/utilites.js
+++ js/utilites.js
@@ -1,4 +1,34 @@
-function showSectionById(id){
+function GetInputFieldValueById(id) {
+    const inputNumber = parseFloat(document.getElementById(id).value);
+    return inputNumber;
+}
+
+
+
+
+function GetTextFieldValueById(id) {
+    const textNumber = parseFloat(document.getElementById(id).value);
+    return textNumber;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function showSectionById(id) {
 
     document.getElementById('donate-section').classList.add('hidden')
     document.getElementById('history-section').classList.add('hidden')
@@ -8,7 +38,7 @@ function showSectionById(id){
 
 
 
-function showButtonById(id){
+function showButtonById(id) {
 
     document.getElementById('show-donate-section').classList.remove('bg-lime-300')
     document.getElementById('show-history-section').classList.remove('bg-lime-300')
