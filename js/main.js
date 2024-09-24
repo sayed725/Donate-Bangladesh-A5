8fb20436abb33ffebe5b0c466a9b1da529b7c0db
sayed725 <ssayed72533@gmail.com>
2024-09-24 06:58:20 +0600

    added history section function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-const myBalance = GetTextFieldValueById('my-balance')
-const donation = GetTextFieldValueById('donation')
-const myDonation = GetInputFieldValueById('my-donation')
-
-
-
 document.getElementById('donate-now').addEventListener('click',function(){
     const myBalance = GetTextFieldValueById('my-balance')
     const donation = GetTextFieldValueById('donation')
@@ -23,6 +17,17 @@ const myNewBalance = myBalance - myDonation
 document.getElementById('my-balance').innerText = myNewBalance.toFixed(2); 
 
 document.getElementById('my_modal_1').showModal();
+
+
+const historySection = document.createElement('div');
+historySection.className = 'text-start px-10 py-6 border-2 border-slate-200 rounded-lg mt-5'
+historySection.innerHTML =`
+
+<h5 class="title font-bold mb-4">${myDonation} Taka is Donated for famine-2024 at Feni, Bangladesh</h5>
+ <p class="normal bg-slate-100">${new Date().toDateString()} ${new Date().toTimeString()} </p>
+`
+
+document.getElementById('history-section').appendChild(historySection);
 
 
 
